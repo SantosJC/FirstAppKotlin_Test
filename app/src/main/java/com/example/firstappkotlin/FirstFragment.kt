9ed3ae65732be6ccb067abbf06e5833389c34ff1
edelossantos <edelossantos@polyworrkmexico.com>
2021-01-29 12:38:22 -0600

    Initial commit

---
+++ app/src/main/java/com/example/firstappkotlin/FirstFragment.kt
@@ -0,0 +1,65 @@
+package com.example.firstappkotlin
+
+import android.os.Bundle
+import android.view.Gravity
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.random_button).setOnClickListener {
+            val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+            val currentCount = showCountTextView.text.toString().toInt()
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(currentCount)
+            findNavController().navigate(action)
+        }
+
+        view.findViewById<Button>(R.id.toast_button).setOnClickListener {
+
+            val base = view.findViewById<ConstraintLayout>(R.id.main_view)
+
+            val mensaje = Snackbar.make(base, "Hello. This is my first Snackbar in Kotlin", Snackbar.LENGTH_SHORT)
+            mensaje.show()
+
+        }
+
+        view.findViewById<Button>(R.id.count_button).setOnClickListener {
+            countMe(view)
+        }
+    }
+
+    private fun countMe(view: View){
+        //Get the text view
+        val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+        //Get the value of the text view
+        val countString = showCountTextView.text.toString()
+        //Convert to INT
+        var count = countString.toInt()
+        count++
+        //Show the next value
+        showCountTextView.text = count.toString()
+    }
+
+}
